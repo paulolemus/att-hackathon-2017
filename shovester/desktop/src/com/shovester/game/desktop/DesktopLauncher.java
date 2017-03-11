5ec136ba75418ab2ccaecd89f7aa36f01c3e2021
Austin Tostito <compactcpp@gmail.com>
2017-03-10 21:42:49 -1000

    Initial commit of libGDX template project

---
+++ shovester/desktop/src/com/shovester/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shovester.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shovester.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
