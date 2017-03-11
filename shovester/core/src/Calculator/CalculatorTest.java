abe602590149638b3eb859e3f8d1ea8de3cb1993
Austin Tostito <compactcpp@gmail.com>
2017-03-10 22:06:36 -1000

    Added Gradle support for Junit

---
+++ shovester/core/src/Calculator/CalculatorTest.java
@@ -0,0 +1,16 @@
+package Calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void evaluatesExpression() {
+		Calculator calculator = new Calculator();
+		int sum = calculator.evaluate("1+2+3");
+		assertEquals(6, sum);
+	}
+
+}
---
+++ shovester/core/src/Calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author aytas
+ *
+ */
+package Calculator;
