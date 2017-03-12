75b1b3c4fa437746d96a2fab2c68b1474f25c0ac
Jared Shimabukuro <jshima@hawaii.edu>
2017-03-11 17:28:01 -1000

    Edited Angle Calculator

--- shovester/src/shovester/calculateAngleToOther.h
+++ shovester/src/shovester/calculateAngleToOther.h
@@ -9,13 +9,13 @@ double calculateAngleToOther(
     double x1, double y1,
     double x2, double y2
 ) {
-   if(x1 > 180) x1 -= 180;
-   if(x2 > 180) x2 -= 180;
-   if(y1 > 180) y1 -= 180;
-   if(y2 > 180) y2 -= 180;
-
    double rad = std::atan((y2 - y1) / (x2 - x1));
-   return ((rad*180)/3.14);
+   double deg = (rad*180)/3.14;
+
+   if(deg > 90) deg -= 180;
+   if(deg < -90) deg += 180;
+
+   return deg;
 }
 
 
